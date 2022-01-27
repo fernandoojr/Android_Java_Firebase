28fa181e10bc50f6cf57567a9e0d5ed2ec014bc9
fernandofojr <fernandofojr@gmail.com>
2022-01-27 19:20:00 -0300

    Commit inicial.

---
+++ app/src/main/java/com/cursoandroid/firebaseapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cursoandroid.firebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*DatabaseReference usuarios = referencia.child("usuarios");
+        Usuario usuario = new Usuario();
+        usuario.setNome("Marcia");
+        usuario.setSobrenome("Santos");
+        usuario.setIdade(43);
+        usuarios.child("002").setValue(usuario);
+
+         */
+        DatabaseReference produtos = referencia.child("produtos");
+        Produto produto = new Produto();
+        produto.setDescricao("Iphone");
+        produto.setMarca("Apple");
+        produto.setPreco(3599.00);
+        produtos.child("001").setValue(produto);
+    }
+}
---
+++ app/src/main/java/com/cursoandroid/firebaseapp/Produto.java
@@ -0,0 +1,33 @@
+package com.cursoandroid.firebaseapp;
+
+public class Produto {
+    String descricao, marca;
+    Double preco;
+
+    public Produto() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
